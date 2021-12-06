915cbccf04c2ad38587de9b0ab8619a5fbd6e96f
Shovek Basu <srb92424@uga.edu>
2021-12-06 15:55:46 -0500

    changed the file the game was in to the OmegaApp java file

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
-javac -d bin -p $JAVAFX_HOME/lib --add-modules javafx.controls pong/Pong.java
-java -cp bin -Dprism.order=sw -p $JAVAFX_HOME/lib --add-modules javafx.controls pong.Pong
+javac -d bin -p $JAVAFX_HOME/lib --add-modules javafx.controls src/main/java/cs1302/omega/OmegaApp.java
+java -cp bin -Dprism.order=sw -p $JAVAFX_HOME/lib --add-modules javafx.controls cs1302.omega.OmegaApp
